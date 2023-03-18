1d4d2d0dedb8bd592316c03a643f278a1695626b
Manhj <manhdd@perfin.io>
2023-03-19 01:04:36 +0700

    up up up

--- src/api/main.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-const API_key = "c704f6fc7c1f4dcc436973235c0e428c";
-export const fetchDataWeather = async (query) => {
-  const { data } = await axios.get(
-    `https://api.openweathermap.org/data/2.5/weather?q=${query}&appid=${API_key}&lang=vi`
-  );
-  return data;
-};
